df7d86b9bbd31d50a9c241716ce362127f313127
Arielle ross <arielle@arielleross.com>
2020-11-23 16:35:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,8 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-return programmer_hash[:dennis_ritchie][:languages]
+language= programmer_hash[:dennis_ritchie][:languages]
+return language
 
 end
 
