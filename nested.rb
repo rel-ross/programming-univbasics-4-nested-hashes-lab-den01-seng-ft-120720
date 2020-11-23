f4eafc9d43a5db645a270489494a8570219fdf2b
Arielle ross <arielle@arielleross.com>
2020-11-23 16:54:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -123,7 +123,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-    programmer_hash[:dennis_ritchie][:languages].push = "Assembly"
+    programmer_hash[:dennis_ritchie][:languages].push("Assembly")
     return programmer_hash
 
 end
