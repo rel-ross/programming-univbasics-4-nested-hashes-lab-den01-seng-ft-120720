a8f6d365ad4846a14898359a3d771643f8703e9d
Arielle ross <arielle@arielleross.com>
2020-11-23 16:51:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -78,11 +78,9 @@ def adding_matz
         :languages => ["C"]
       }
     }
-programmer_hash[:yukihiro_matsumoto]
-programmer_hash[:yukihiro_matsumoto][:known_for]
-programmer_hash[:yukihiro_matsumoto][:known_for]["Ruby"]
-programmer_hash[:yukihiro_matsumoto][:languages]["LISP"]
-programmer_hash[:yukihiro_matsumoto][:languages]["C"]
+programmer_hash[:yukihiro_matsumoto] = {}
+programmer_hash[:yukihiro_matsumoto][:known_for]= "Ruby"
+programmer_hash[:yukihiro_matsumoto][:languages]= ["LISP", "C"]
 return programmer_hash
 end
 
